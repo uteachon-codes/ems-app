d69dc13495a3fe5d595e2e268ad70e7b6491e97c
Hussain Ashruf <uteachon-codes@gmail.com>
2021-03-16 10:40:17 -0600

    added tests

--- src/test/java/com/uteachon/apps/ems/EmsAppApplicationTests.java
+++ src/test/java/com/uteachon/apps/ems/EmsAppApplicationTests.java
@@ -1,5 +1,6 @@
 package com.uteachon.apps.ems;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class EmsAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Triggering EmsAppApplicationTests ");
+		Assertions.assertEquals(true, true);
 	}
 
 }
